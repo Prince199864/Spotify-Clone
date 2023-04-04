a4fc69b4bd27d9967920e7e63862ec4d4f759bc5
Prince199864 <princejose199864@gmail.com>
2023-04-04 21:58:02 +0530

    Initial commit

---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+let songs = [
+    {songName: "Test1", filePath:"/songs/1.mp3", coverPath:"/Images/1.jpg"},
+    {songName: "Test1", filePath:"/songs/2.mp3", coverPath:"/Images/3.jpg"},
+    {songName: "Test1", filePath:"/songs/3.mp3", coverPath:"/Images/3.jpg"},
+    {songName: "Test1", filePath:"/songs/4.mp3", coverPath:"/Images/4.jpg"},
+    {songName: "Test1", filePath:"/songs/5.mp3", coverPath:"/Images/5.jpg"},
+    {songName: "Test1", filePath:"/songs/6.mp3", coverPath:"/Images/6.jpg"},
+    {songName: "Test1", filePath:"/songs/7.mp3", coverPath:"/Images/7.jpg"},
+    {songName: "Test1", filePath:"/songs/8.mp3", coverPath:"/Images/8.jpg"},
+];
+let audioElement= new Audio('/songs/1.mp3');
+// audioElement.play()
